3930eaf12c1620e3bbb1a18fe8328dc3ff81d840
Charles Wang <charleswang007@gmail.com>
2021-10-24 07:28:18 +0800

    add program code

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            Console.WriteLine($"\nHello {name}!");
+            Console.WriteLine("Hello World!");
+
+            DateTime now = DateTime.Now;
+            int dayOfYear = now.DayOfYear;
+            Console.Write("Day of year: ");
+            Console.WriteLine(dayOfYear);
+        }
+    }
+}
